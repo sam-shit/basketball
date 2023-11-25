f18e55c6422c0d08c9363aa89c7217b040890ff3
Iamneo <techadmin@iamneo.ai>
2023-11-25 10:53:09 +0000

    Backup Commit

--- angularapp/src/app/players/details/details.component.ts
+++ angularapp/src/app/players/details/details.component.ts
@@ -1,3 +1,4 @@
+import { Position } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Player } from 'src/app/models/player';
 import { PlayersService } from 'src/app/services/players.service';
@@ -12,7 +13,14 @@ export class DetailsComponent implements OnInit {
   constructor(private ms : PlayersService) { }
 
   playerData : any[] = []
-  player : any
+  player : {id : number, name : string, shirtno : number, position : Position, appearances : number, goals : number} = {
+    id: 1,
+    name: 'Test Player',
+    shirtno: 10,
+    position: { id: 1, name: 'Forward' },
+    appearances: 20,
+    goals: 10,
+  }
 
   getPlayers() : any[] {
     this.ms.getPlayers().subscribe(data => {this.playerData.push(...data)})
